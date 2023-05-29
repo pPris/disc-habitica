602185db905b996c8aa2b8def3b338d657145c52
priscilla <49683462+pPris@users.noreply.github.com>
2023-05-29 19:28:32 +0800

    init

---
+++ discord_manager.py
@@ -0,0 +1,33 @@
+import requests
+from secrets_1 import token_secret
+import random
+work_ti = '875294338622570587' # for sanity testing
+
+def get_channel_api_string(channel_id: int):
+    channel_string_start = "https://discord.com/api/v9/channels/"
+    channel_string_end = "/messages"
+    return (channel_string_start + str(channel_id) + channel_string_end)
+
+def getChannelName(channelID: str):
+    authToken = token_secret
+    header = {"authorization": authToken}
+    res = requests.get(url=f'https://discord.com/api/v9/channels/{channelID}', headers=header)
+    try:
+        return res.json()["name"] 
+    except KeyError:
+        return res.json()["recipients"][0]["username"]
+    except:
+        return "can't find channel name"
+
+# class SendMessage:
+def sendMessage(channelID: str, payload: str, authToken: str=token_secret):
+    payload = {"content": payload}
+    header = {"authorization": authToken}
+    res = requests.post(get_channel_api_string(channelID), data=payload, headers=header)
+    print(f'message was sent to channelID: {getChannelName(channelID)} with code {res.status_code}')
+
+# simpleFormat = f'>>> {content} \n *sent by {sender}*'
+
+def fwdToDiscordParty(formatted: str, unformattedVersion:str, sender: str):
+    message = f'```\n{unformattedVersion}```*-{sender}*'
+    sendMessage(work_ti, message)
---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+from discord_manager import fwdToDiscordParty
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello world', 200
+
+# URI for receiving inbound webhook request
+@app.route('/', methods=['POST'])
+def webhook():
+    # Request contains data as shown at https://habitica.com/apidoc/#api-Webhook
+     # .json converts request to Python dictionary
+    print(request.json)  # type: dict
+    res = request.json
+
+    # <YOUR CODE FOR HANDLING EVENT HERE>
+    formattedText = res['chat']['text']
+    unformattedText = res['chat']['unformattedText']
+    
+    isSystem = res['chat']['uuid'] == 'system'
+    if (isSystem): 
+        sender = ("system")
+    else: 
+        sender = res['chat']['user']
+
+    fwdToDiscordParty(formattedText, unformattedText, sender)
+    # print(res['chat']['info']['user'])
+ 
+    return '', 200 # Return 200 code to the sending webserver
+    
+ 
+if __name__ == "__main__":
+    app.run()
+    # app.run(host='0.0.0.0', port='<your_port>')
